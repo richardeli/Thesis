12dad7ec708722a9e545323afe92479f6d4dc181
Richarde <rili4543@uni.sydney.edu.au>
2024-10-21 09:55:53 +1100

    test

--- Market/run.py
+++ Market/run.py
@@ -2,7 +2,7 @@ from systembook import SystemBook
 
 def test_book():
     for num_sims in range(1):
-        for num_runs in range(1000):
+        for num_runs in range(10):
             sys = SystemBook(num_init_fundamentalists=900, num_init_speculators=1600, init_upward_market_dir=False, 
                             num_trade_cycles=45, chg_num_agent_pcycle=1, cycle_cool_off_per_dilution=1, 
                             fund_dilute_rm=False, change_dilution_dir_cycle_num=None)
