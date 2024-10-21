bc6b96abe594a63d752dfd2cc18a7f92426b428b
Richarde Li <rili4543@uni.sydney.edu.au>
2024-10-21 22:03:43 +1100

    d

--- Market/run.py
+++ Market/run.py
@@ -7,8 +7,8 @@ def test_book():
                             num_trade_cycles=45, chg_num_agent_pcycle=1, cycle_cool_off_per_dilution=1, 
                             fund_dilute_rm=False, change_dilution_dir_cycle_num=None)
             sys.trade_cycle()
-            # sys.output_graph()
-            sys.save_to_excel(num_sims+1, num_runs+1)
+            sys.output_graph()
+            # sys.save_to_excel(num_sims+1, num_runs+1)
         
 if __name__ == '__main__':
     test_book()
